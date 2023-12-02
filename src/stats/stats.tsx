e4d13054fbc7d81b998f8a83cb28bde089a33a4f
Tom Rooman <tomrooman@hotmail.com>
2023-12-02 23:52:28 +0100

    feat: stats for trades in table format

--- src/stats/stats.tsx
+++ src/stats/stats.tsx
@@ -1,23 +1,35 @@
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import StarBorder from '@mui/icons-material/StarBorder';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Collapse from '@mui/material/Collapse';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import axios from 'axios';
+import * as React from 'react';
 import { useEffect, useRef, useState } from 'react';
 
-import styles from './stats.module.scss';
+const tableContentStyle = {
+    'padding': "4px 16px 4px 16px",
+};
 
-const YearComponent = ({ stats, year, month, day }: { stats: any, year: number, month?: number, day?: number }) => {
+const tableHeaderStyle = {
+    'padding': "16px 16px 16px 16px",
+    'fontWeight': 'bold',
+    'backgroundColor': '#f5f5f5',
+};
+
+const Row = ({ stats, year, month, day }: { stats: any, year: number, month?: number, day?: number }) => {
     const [open, setOpen] = useState(false);
 
     let selectedData = stats.find((d: any) => d.value === year);
     let selectedType = 'Année';
     let selectedValue = year;
+
     if (month) {
         selectedData = selectedData.months.find((d: any) => d.value === month);
         selectedType = 'Mois';
@@ -38,27 +50,66 @@ const YearComponent = ({ stats, year, month, day }: { stats: any, year: number, 
     }
 
     return (
-        <>
-            <ListItemButton onClick={() =>
-                setOpen(!open)
-            }>
-                <ListItemIcon>
-                    <InboxIcon />
-                </ListItemIcon>
-                <ListItemText primary={selectedType + '' + selectedValue} />
-                {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse className={styles[selectedType + "_content"]} in={open} timeout="auto" unmountOnExit>
-                {selectedData.map((d: any, index: number) => selectedType !== 'Jour' ? <YearComponent key={year.toString() + d.value.toString()} stats={stats} year={year} month={month || d.value} day={month ? d.value : null} /> : <List key={year.toString() + month?.toString() + day?.toString() + index.toString()} component="div">
-                    <ListItemButton sx={{ pl: 4 }}>
-                        <ListItemIcon>
-                            <StarBorder />
-                        </ListItemIcon>
-                        <ListItemText primary="Starred" />
-                    </ListItemButton>
-                </List>)}
-            </Collapse>
-        </>)
+        <React.Fragment>
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                <TableCell sx={tableContentStyle}>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell sx={tableContentStyle} scope="row">
+                    {selectedValue}
+                </TableCell>
+                <TableCell sx={tableContentStyle} align="right">{selectedData.profit}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ padding: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        {selectedType !== 'Jour' ?
+                            <Table style={{ "display": "inline-table", "width": "100%" }}>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell sx={tableHeaderStyle} />
+                                        <TableCell sx={tableHeaderStyle}>{selectedType === "Année" ? 'Mois' : 'Jour'}</TableCell>
+                                        <TableCell sx={tableHeaderStyle} align="right">Profit</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {selectedData.map((d: any) => <Row key={year.toString() + d.value.toString()} stats={stats} year={year} month={month || d.value} day={month ? d.value : day} />)}
+                                </TableBody>
+                            </Table>
+                            : false}
+                        {selectedType === 'Jour' ?
+                            <Table style={{ "display": "inline-table", "width": "100%" }}>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell sx={tableHeaderStyle}>Ouverture</TableCell>
+                                        <TableCell sx={tableHeaderStyle}>Fermeture</TableCell>
+                                        <TableCell sx={tableHeaderStyle} align="right">Profit</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                {selectedData.map((d: any) =>
+                                    <TableBody key={d.opened_at}>
+                                        <TableRow >
+                                            <TableCell sx={tableContentStyle} scope="row">
+                                                {d.opened_at}
+                                            </TableCell>
+                                            <TableCell sx={tableContentStyle}>{d.closed_at}</TableCell>
+                                            <TableCell sx={tableContentStyle} align="right">{d.profit}</TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                )}
+                            </Table>
+                            : false}
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment >
+    );
 }
 
 const Stats = () => {
@@ -75,31 +126,27 @@ const Stats = () => {
 
     const synchronizeData = async () => {
         const { data } = await axios.get(`http://localhost:5000/stats`);
-        // const formattedCandlesData = data.candles.map((d: any) => ({
-        //     open: d.open,
-        //     high: d.high,
-        //     low: d.low,
-        //     close: d.close,
-        //     time: Math.round(d.start_timestamp / 1000)
-        // }));
         setStats(data);
         console.log({ data })
     }
 
+
     return (
-        <div style={{
-            display: 'flex',
-            height: '100%',
-        }}>
-            <List
-                sx={{ width: '100%', maxWidth: '50%', bgcolor: 'background.paper' }}
-                component="nav"
-                aria-labelledby="nested-list-subheader"
-            >
-                {stats.map((d: any) => <YearComponent key={d.value} stats={stats} year={d.value} />)}
-            </List>
-        </div >
-    )
+        <TableContainer component={Paper}>
+            <Table aria-label="collapsible table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell sx={tableHeaderStyle} />
+                        <TableCell sx={tableHeaderStyle}>Année</TableCell>
+                        <TableCell sx={tableHeaderStyle} align="right">Profit</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {stats.map((d: any) => <Row key={d.value} stats={stats} year={d.value} />)}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
 
-export default Stats
+export default Stats;
